8b6264996755652cc48801aa44c577bc0f4e8072
takonomura <takonomura@users.noreply.github.com>
2021-12-26 17:04:22 +0900

    Add script to extract kindle content list

---
+++ kindle-list/extract.py
@@ -0,0 +1,19 @@
+import sys
+import json
+from warcio.archiveiterator import ArchiveIterator
+
+items = []
+
+with open(sys.argv[1], 'rb') as stream:
+    for record in ArchiveIterator(stream):
+        if record.rec_type != 'response':
+            continue
+        if record.rec_headers.get_header('WARC-Target-URI') != 'https://www.amazon.co.jp/hz/mycd/ajax':
+            continue
+        data = json.loads(record.content_stream().read())
+        if not 'OwnershipData' in data:
+            continue
+        for item in data['OwnershipData']['items']:
+            items.append(item)
+
+print(json.dumps(items))
